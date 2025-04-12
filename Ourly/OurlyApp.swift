3a181542c7bbb2f2493ddc459ec46d617b38ab46
Bear-sys <seanrhee8@gmail.com>
2025-04-11 17:21:30 -0700

    Initial Commit

---
+++ Ourly/OurlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  OurlyApp.swift
+//  Ourly
+//
+//  Created by Sean Rhee on 4/11/25.
+//
+
+import SwiftUI
+
+@main
+struct OurlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
