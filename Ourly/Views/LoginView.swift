d6f16aa04053bd2b56fad4b17a4d37ad17f4ea6e
Bear-sys <seanrhee8@gmail.com>
2025-04-14 14:33:52 -0700

    Current updated loginview

--- Ourly/Views/LoginView.swift
+++ Ourly/Views/LoginView.swift
@@ -13,96 +13,99 @@ import FirebaseAuth
 struct LoginView: View {
     @State private var email = ""
     @State private var password = ""
+    @State private var isLoggedIn = false
+    
+    init() {
+            if Auth.auth().currentUser != nil {
+                _isLoggedIn = State(initialValue: true)
+            }
+        }
     
     var body: some View {
         NavigationStack {
-            ZStack {
-                // background image
-                Color(.white)
-                    .ignoresSafeArea()
-                
-                VStack(spacing: 20) {
-                    // image
-                    Spacer()
-                    Image("Open Screen")
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: 400, height: 400)
+            if isLoggedIn {
+                FeedView()
+            } else {
+                ZStack {
+                    // background image
+                    Color(.white)
+                        .ignoresSafeArea()
                     
-                    
-                    // form fields
-                    VStack(spacing: 16) {
+                    VStack(spacing: 20) {
+                        // image
+                        Spacer()
+                        Image("Open Screen")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 400, height: 400)
                         
-                        TextField("Email", text: $email)
-                            .textInputAutocapitalization(.never)
-                            .keyboardType(.emailAddress)
-                            .padding()
-                            .background(Color.white)
-                            .cornerRadius(8)
-                            .padding(.horizontal, 32)
                         
-                        SecureField("Password", text: $password)
-                            .padding()
-                            .background(Color.white)
-                            .cornerRadius(8)
-                            .autocapitalization(.none)
-                            .padding(.horizontal, 32)
-                    }
-                    .padding(.horizontal, 32)
-                    
-                    //sign-in button
-                    Button(action: {
-                        Auth.auth().signIn(withEmail: email, password: password) {
-                            authResult, error in
-                            if let error = error {
-                                print("Sign-in failed: \(error.localizedDescription)")
-                            } else {
-                                print("Signed in successfully")
-                                //move to the feed view
-                            }
+                        // form fields
+                        VStack(spacing: 16) {
+                            
+                            TextField("Email", text: $email)
+                                .textInputAutocapitalization(.never)
+                                .keyboardType(.emailAddress)
+                                .padding()
+                                .background(Color.white)
+                                .cornerRadius(8)
+                                .padding(.horizontal, 32)
+                            
+                            SecureField("Password", text: $password)
+                                .padding()
+                                .background(Color.white)
+                                .cornerRadius(8)
+                                .autocapitalization(.none)
+                                .padding(.horizontal, 32)
                         }
-                    }) {
-                        Text("Sign-in")
-                            .foregroundColor(.white)
-                            .padding()
-                            .frame(maxWidth: .infinity)
-                            .background(Color(red: 19/255, green: 46/255, blue: 117/255))
-                            .cornerRadius(8)
-                            .padding(.horizontal, 32)
-                    }
-                    .padding(.horizontal, 32)
-                    .padding(.top, 10)
-                    
-                    //sign-up button
-                    Button(action: {
-                        Auth.auth().createUser(withEmail: email, password: password) {
-                            authResult, error in
-                            if let error = error {
-                                print("Sign-up failed: \(error.localizedDescription)")
-                            } else {
-                                print("Account created successfully")
+                        .padding(.horizontal, 32)
+                        
+                        //sign-in button
+                        Button(action: {
+                            Auth.auth().signIn(withEmail: email, password: password) {
+                                authResult, error in
+                                if let error = error {
+                                    print("Sign-in failed: \(error.localizedDescription)")
+                                } else {
+                                    print("Signed in successfully")
+                                    isLoggedIn = true
+                                    //move to the feed view
+                                }
                             }
+                        }) {
+                            Text("Sign-in")
+                                .foregroundColor(.white)
+                                .padding()
+                                .frame(maxWidth: .infinity)
+                                .background(Color(red: 19/255, green: 46/255, blue: 117/255))
+                                .cornerRadius(8)
+                                .padding(.horizontal, 32)
                         }
-                }) {
-                    Text("Don't have an account? Sign-up")
-                        .foregroundColor(.white)
-                        .font(.footnote)
-                }
-                .padding(.top, 8)
+                        .padding(.horizontal, 32)
+                        .padding(.top, 10)
+                        
+                        //sign-up button
+                        Button(action: {
+                            Auth.auth().createUser(withEmail: email, password: password) {
+                                authResult, error in
+                                if let error = error {
+                                    print("Sign-up failed: \(error.localizedDescription)")
+                                } else {
+                                    print("Account created successfully")
+                                }
+                            }
+                        }) {
+                            Text("Don't have an account? Sign-up")
+                                .foregroundColor(.white)
+                                .font(.footnote)
+                        }
+                        .padding(.top, 8)
+                    }
                 }
             }
         }
     }
 }
-
-
-
-
-
-
-
-
-
 
 
 
